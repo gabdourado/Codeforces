6df5f68cb808da609f23b014a2cd17606bf79994
gabdourado <gab.dourado.if23@gmail.com>
2025-09-20 19:15:49 -0300

    Solução comendata do problema 110A

--- Problems/800/110A.cpp
+++ Problems/800/110A.cpp
@@ -1,3 +1,14 @@
+/*
+    Nome: Gabriel Dourado
+
+    Solução: Um número quase da sorte é um número que tem 4 dígitos 7 e/ou 4 na sua formação ou um número que
+    tem 7 dígitos 4 e/ou 7 na sua formação. Exemplo de números quase da sorte: 77144, 770077, 74847774.
+
+    Assim, vamos contar quantos 4's e 7's aparecem nos números e verificar se essa quantidade é igual a 4
+    ou se é igual a 7, mostrando assim a a saída esperada.
+
+*/
+
 #include <bits/stdc++.h>
 
 using namespace std;
