0b3e5cc289ded2d21172880964289670a39cb94e
gabdourado <gab.dourado.if23@gmail.com>
2025-09-20 19:28:54 -0300

    Solução comentada do problema 112A

--- Problems/800/112A.cpp
+++ Problems/800/112A.cpp
@@ -1,3 +1,15 @@
+/*
+    Nome: Gabriel Dourado
+
+    Solução: Dadas duas strigns S e T, S é dita menor que T, em ordem lexicográfica, se existe um i, tal que 
+    S[i] < T[i], ou seja, o caractere da posição i de S aparece primeiro queo caractere da posição i de T. O 
+    mesmo ocorre para uma string S ser maior que T. Duas strings S e T são iguais S[i] == S[i] para todo i.
+    
+    Assim, vamos percorrer S e T e verificar se em algum momento S[i] < T[i], pois isso significa que S é menor
+    que T, ou se S[i] > T[i], pois então S é maior que T ou se ambos os casos não ocorrem, S é igual a T.
+
+*/
+
 #include <bits/stdc++.h>
 
 using namespace std;
