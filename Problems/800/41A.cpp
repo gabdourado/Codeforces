80f6fd582e57235996f0c7e769bd9fb77558dc64
gabdourado <gab.dourado.if23@gmail.com>
2025-09-20 16:09:46 -0300

    Solução comentada do problema 41A

--- Problems/800/41A.cpp
+++ Problems/800/41A.cpp
@@ -1,10 +1,21 @@
 /*
     Nome: Gabriel Dourado
 
-    Solução: 
+    Solução: O clássico problema de verificar se dada uma string S, T é a sua inversão. Primeiro verificamos
+    os tamanhos das duas strings S e T, se são diferentes, T já não pode ser a inversa de S. Caso os tamanhos
+    forem iguais, partimos para a comparação. Para o caso base número 1:
 
+    S = | c | o | d | e |
+        | 0 | 1 | 2 | 3 |
+
+    T = | e | d | o | c |
+        | 0 | 1 | 2 | 3 |
+
+    Verificamos se a posição S[0] é igual a T[3], T[1] é igual a S[2], ... S[i] é igual a T[size - i - 1], onde
+    size é o tamanhos strings. Se em algum momento houver diferença, T não é a string inversa de S.
 
 */
+
 #include <bits/stdc++.h>
 
 using namespace std;
