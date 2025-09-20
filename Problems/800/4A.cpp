a3e2e540d7e3f38de8ee2accce1de97929989dde
gabdourado <gab.dourado.if23@gmail.com>
2025-09-20 15:58:45 -0300

    Adicionando solução com nome

--- Problems/800/4A.cpp
+++ Problems/800/4A.cpp
@@ -1,3 +1,15 @@
+/*
+    Nome: Gabriel Dourado
+
+    Solução: Esse problema é ralativamente fácil. Dado que W é um número inteiro positivo, só sendo um número 
+    par para poder ser escrito como a soma de dois outros números pares. É evidente que por se tratar de uma 
+    melancia, não podemos ter uma parcela valendo zero nessa soma. Isso ocorre quando W = 2, pois em tese a 
+    soma seria 0 + 2, o que não ée permitido. 
+    
+    Assim, é possível divir a melancia sempre que W for par e diferente de 2.
+
+*/
+
 #include <bits/stdc++.h>
 
 using namespace std;
