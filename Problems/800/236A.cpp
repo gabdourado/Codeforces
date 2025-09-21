c05ea3985c30e99c0e2fe0c6f9e00695d5c7e245
gabdourado <gab.dourado.if23@gmail.com>
2025-09-21 11:32:08 -0300

    Solução comentada do problema 236A

--- Problems/800/236A.cpp
+++ Problems/800/236A.cpp
@@ -1,3 +1,17 @@
+/*
+    Nome: Gabriel Dourado
+
+    Solução: Dada uma string S, queremos saber a quantidade de caracteres distintos de S. Se esse número for 
+    par, então é uma mulher, caso contrário, é um homem. Observe o caso base:
+
+    S = | w | j | m | z | b | m | r |
+
+    As letras distintas são: {w, j, m, z, b, r}. Como o problema é de identificar o número de objetos distintos,
+    vamos usar um set para guardar os elementos e ao final, contar a quantidade de elementos, e verirficando se
+    é par ou ímpar, mostrar a saída esperada.
+
+    */
+
 #include <bits/stdc++.h>
 
 using namespace std;
