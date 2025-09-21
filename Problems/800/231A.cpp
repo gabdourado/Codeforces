6a5bef830bd9e019794b0eac7392d1ce3aa37940
gabdourado <gab.dourado.if23@gmail.com>
2025-09-21 11:24:55 -0300

    Solução comentada do problema 231A

--- Problems/800/231A.cpp
+++ Problems/800/231A.cpp
@@ -1,3 +1,12 @@
+/*
+    Nome: Gabriel Dourado
+
+    Solução: O que importa para nós são os problemas onde a soma dos seus status é maior que 1, ou seja, onde
+    ao menos duas pessoas sabem a solução. Dessa forma, em cada linha vamos somar esses valores e verificar se
+    é maior que 1, caso sim, esse problema é contado. Ao final, teremos quantos problemas elas sabem resolver.
+
+*/
+
 #include <bits/stdc++.h>
 
 using namespace std;
