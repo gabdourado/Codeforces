7730e05d991231ef1bfcea1a3b74a7e749c2c1d5
gabdourado <gab.dourado.if23@gmail.com>
2025-09-23 09:16:41 -0300

    Solução comentada do problema 136A

--- Problems/800/136A-Presents.cpp
+++ Problems/800/136A-Presents.cpp
@@ -1,8 +1,22 @@
 /*
     Nome: Gabriel Dourado
 
-    Solução:
+    Solução: O problema nos diz que o i-ésimo elemento, ou seja, p[i], deu presente para o amigo i.
+    Como mostra o caso base abaixo:
 
+    p = | 2 | 3 | 4 | 1 | -> Deu presente
+        | 1 | 2 | 3 | 4 | -> Recebeu presente
+
+    Queremos saber para quem i deu presente, então vamos construir um vetor p' com essa associação,
+    criado do seguinte modo:
+
+    p'[p[i]] = i
+
+    Assim, a saída seria:
+
+    p' = | 4 | 1 | 2 | 3 |
+         | 1 | 2 | 3 | 4 |
+         
 */
 
 #include <bits/stdc++.h>
