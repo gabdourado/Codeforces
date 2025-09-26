dcf4408bef5f8c9213d5c7871565d8614ddc170c
gabdourado <gab.dourado.if23@gmail.com>
2025-09-26 08:44:49 -0300

    Solução comentada do problema 510

---
+++ Problems/0500-0599/510A-Fox-And-Snake.cpp
@@ -0,0 +1,43 @@
+/*
+    Nome: Gabriel Dourado
+
+    Solução: Para esse problema, vamos analisar o seguinte, se é uma linha par, será composta por m "#",
+    já se é uma linha impar, poderá ser composta por (m - 1) "." e um a"#" ou o a mesma quantida, porém 
+    na ordem inversa. Para cuidar dessa inversão, vamos criar uma variável para sabermos qual sentido adotar. 
+    Em seguida, só implementar esse algoritmo. 
+
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main (void) {
+
+    int N, M;
+    bool right(false);
+
+    cin >> N >> M;
+
+    for (int i = 0; i < N; i++) {
+
+        if (i % 2 != 0) {
+            if (right) {
+                cout << "#";
+                for (int j = 0; j < M - 1; j++) cout << ".";
+                cout << endl;
+                right = false;
+            } else {
+                for (int j = 0; j < M - 1; j++) cout << ".";
+                cout << "#" << endl;
+                right = true;
+            }
+        } else {
+            for (int i = 0; i < M; i++) cout << "#";
+            cout << endl;
+        }
+
+    }
+
+    return 0;
+}
