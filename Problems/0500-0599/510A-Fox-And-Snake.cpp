e210a0ffc76a446b10a0766189a60c0559e861f8
gabdourado <gab.dourado.if23@gmail.com>
2025-09-26 08:50:44 -0300

    Solução comentada do problema 510A

--- Problems/0500-0599/510A-Fox-And-Snake.cpp
+++ Problems/0500-0599/510A-Fox-And-Snake.cpp
@@ -2,9 +2,30 @@
     Nome: Gabriel Dourado
 
     Solução: Para esse problema, vamos analisar o seguinte, se é uma linha par, será composta por m "#",
-    já se é uma linha impar, poderá ser composta por (m - 1) "." e um a"#" ou o a mesma quantida, porém 
-    na ordem inversa. Para cuidar dessa inversão, vamos criar uma variável para sabermos qual sentido adotar. 
-    Em seguida, só implementar esse algoritmo. 
+    já se é uma linha impar, poderá ser composta por (m - 1) "." e uma "#" ou o a mesma quantida, porém 
+    na ordem inversa. Para cuidar dessa inversão, vamos criar uma variável para sabermos se o "vazio"
+    será a direita ou a esquerda. 
+    
+    Observe o caso base 1:
+
+    Entrada (n, m):
+    5 3
+
+    Processo:
+    ### -> Linha 0
+    ..# -> Linha 1 (vazio a esquerda)
+    ### -> Linha 2
+    #.. -> Linha 3 (vazio a direita)
+    ### -> Linha 4 
+
+    Saída:
+    ###
+    ..#
+    ###
+    #..
+    ###
+    
+    Agora é só implementar esse algoritmo.
 
 */
 
