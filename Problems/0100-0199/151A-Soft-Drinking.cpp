0e516368006401161431251ceaf025d077d8835a
gabdourado <gab.dourado.if23@gmail.com>
2025-09-29 21:38:34 -0300

    Solução do problema 151A

---
+++ Problems/0100-0199/151A-Soft-Drinking.cpp
@@ -0,0 +1,61 @@
+/*
+    Nome: Gabriel Dourado
+
+    Solução: Vamos compreender o que cada variável representa para o problema, assim fica mais fácil de pensar
+    em uma solução simples. Sejam as variáveis:
+
+    n := Quantidade de amigos
+    k := Quantidade de garrafas
+    l := Quantidade de mililitros de cada garrafa
+    c := Quantidade de limas
+    d := Quantidade de fatias de cada lima
+    p := Quantidade de sal
+    nl := Quantidade de mililitros de cada drink
+    np := Quantidade de sal de cada fatia
+
+    Para calcular quantos drinks posso fazer, basta fazer:
+
+    (k * l) / nl
+
+    Para calcular quantas fatias de lima temos disponíveis, basta fazer:
+
+    (c * d)
+
+    Para cacularmos a quantidade de "pitadas" de sal disponíveis, basta fazer:
+
+    p / np
+
+    Calculado essas componentes de cada brinde, para saber quantos se pode fazer, basta determinar o
+    mínimo entre essas componestes e fazer esse valor dividido por n, pois essa será a aquantidade de
+    brindes.
+
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int min(int a, int b, int c) {
+    int min;
+    min = a < b ? a : b;
+    min = min < c ? min : c;
+    return min;
+}
+
+int main (void) {
+
+    int n, k, l, c, d, p, nl, np;
+    int toasts, drink, limes, salt;
+
+    cin >> n >> k >> l >> c >> d >> p >> nl >> np;
+
+    drink = (k * l) / nl;
+    limes = (c * d);
+    salt = p / np;
+    
+    toasts = min(drink, limes, salt) / n;
+
+    cout << toasts << endl;
+
+    return 0;
+}
