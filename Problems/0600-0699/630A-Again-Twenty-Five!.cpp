de148c2bb4b6ccf783b0249b2a72273d51b32450
gabdourado <gab.dourado.if23@gmail.com>
2025-10-01 09:45:38 -0300

    Solução do problema 630A

---
+++ Problems/0600-0699/630A-Again-Twenty-Five!.cpp
@@ -0,0 +1,42 @@
+/*
+    Nome: Gabriel Dourado
+
+    Solução: Seria uma zoeira essa questão? É possível provar que para todo n maior que 2, 5^n termina em 25.
+    Queremos porvar que 5^n ≡ 25 (mod 100). Segue a prova por indução:
+
+    Caso base: Para n = 2 temos:
+    
+    5^2 = 25 ≡ 25 (mod 100)
+    
+    Hipótese de Indução: Suponhamos que seja verdade para k, onde 2 <= k <= n, logo segue que:
+
+    5^k ≡ 25 (mod 100)
+
+    Tese: Queremos mostrar que a congruência é válida para (k+1), ou seja:
+    
+    5^(k + 1) ≡ 25 (mod 100)
+
+    Ora,
+
+    5^(k + 1) = 5^k * 5
+              ≡ 25 * 5 (mod 100) (Hipótese)
+              = 125
+              ≡ 25 (mod 100)
+    
+    Logo, o Princípio da Indução Finita garante que a relação é válida para todo n maior ou igual que 2. ■
+
+*/
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main (void) {
+
+    int n;
+
+    cin >> n;
+    cout << 25 << endl;
+
+    return 0;
+}
