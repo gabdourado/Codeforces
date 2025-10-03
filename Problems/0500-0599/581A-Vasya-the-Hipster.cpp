41f9c390abcd39625a7a10706724753e492ae403
gabdourado <gab.dourado.if23@gmail.com>
2025-10-03 08:59:10 -0300

    Solução comentada do problema 581A

---
+++ Problems/0500-0599/581A-Vasya-the-Hipster.cpp
@@ -0,0 +1,44 @@
+/*
+    Nome: Gabriel Dourado
+
+    Solução: Vamos pensar na quantidade de meias diferentes que ele tem. Imagine duas gavetas com uma 
+    quantidade a de meias R (red) e b de meias B (blue). Os pares de meias diferentes vai ser dado por:
+    min(a, b). E a quantidade de pares com meias iguais vai ser dado por: (max(a,b) - min(a, b))/2.
+
+    Observe esse exemplo com a = 5 e b = 3:
+
+    |   R   |
+    |   R   |
+    |   R   |    |   B   |
+    |   R   |    |   B   |
+    |   R   |    |   B   |
+    |_______|    |_______|
+
+    Pares diferentes:
+    min(5, 3) = 3
+
+    Pares iguais:
+    (max(5, 3) - min(5, 3))/2 = (5 - 3)/2 = 1
+
+*/
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int max(int a, int b) {return (a > b ? a : b);}
+int min(int a, int b) {return (a < b ? a : b);}
+
+int main (void) {
+
+    int a, b, d, s;
+
+    cin >> a >> b;
+
+    d = min(a, b);
+    s = (max(a, b) - min(a, b))/2;
+
+    cout << d << " " << s << endl;
+
+    return 0;
+}
